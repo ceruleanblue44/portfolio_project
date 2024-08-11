6ff8b1e37d35e23897c98b959a3f916b9a4de13a
Marina Gotovtceva <ceruleanblue44@gmail.com>
2024-08-11 23:10:49 +0300

    setting colors

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import 'animate.css/animate.min.css'
-import { PerfectScrollbar} from 'vue3-perfect-scrollbar';
-import 'vue3-perfect-scrollbar/style.css';
+import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar/style.css'
 
 import '@/style/style.scss'
 
@@ -11,8 +11,16 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App)
-	.use(PerfectScrollbar)
-	.use(createPinia())
-	.use(router)
-	.mount('#app')
-
+  .use(PerfectScrollbar, {
+    // watchOptions: true,
+    // options: {
+    //   suppressScrollX: true,
+    //   swipeEasing: true,
+    //   wheelPropagation: true,
+    //   maxScrollbarLength: 200,
+    //   wheelSpeed: 0.3
+    // }
+  })
+  .use(createPinia())
+  .use(router)
+  .mount('#app')
